95da8613f9e3e1deb243df0b491a06bc20a47323
kevardila <kevin.ardila@nuamx.com>
2024-09-09 11:08:32 -0500

    feat: testdev

--- test_main.py
+++ test_main.py
@@ -1,23 +1,37 @@
 import pytest
+import allure
 from main import suma, resta, multiplicacion, division
 
+
+@allure.parent_suite("summa suite")
+@allure.suite("numeros positivos")
+@allure.sub_suite("numeros del 1 al 10")
 def test_suma():
     assert suma(1, 2) == 3
-    assert suma(-1, 1) == 0
+    assert suma(2, 3) == 5
     assert suma(0, 0) == 0
 
+@allure.parent_suite("resta suite")
+@allure.suite("numeros positivos")
+@allure.sub_suite("numeros del 1 al 10")
 def test_resta():
     assert resta(3, 2) == 1
     assert resta(-1, 1) == -2
     assert resta(0, 0) == 0
 
+@allure.parent_suite("multiplicacion suite ")
+@allure.suite("numeros positivos")
+@allure.sub_suite("numeros del 1 al 10")
 def test_multiplicacion():
     assert multiplicacion(2, 3) == 6
     assert multiplicacion(-1, 1) == -1
     assert multiplicacion(0, 5) == 0
 
+@allure.parent_suite("Division suite")
+@allure.suite("numeros positivos")
+@allure.sub_suite("numeros del 1 al 10")
 def test_division():
-    assert division(6, 3) == 0
+    assert division(6, 3) == 2
     assert division(-6, 2) == -3
     assert division(0, 1) == 0
     with pytest.raises(ValueError, match="No se puede dividir por cero"):
